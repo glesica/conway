4479ce7660d75ad3a53e98a47efbe1b9a383621d
George Lesica <george@lesica.com>
2023-06-16 10:00:32 -0600

    Switch to health model

--- source/main.lua
+++ source/main.lua
@@ -21,6 +21,11 @@ local size <const> = 8
 local width <const> = 50
 local height <const> = 30
 
+local initialAlive <const> = 0.25
+local maxHealth <const> = 1
+
+local stepTime <const> = 0
+
 local cellStates = table.create(width * height, 0)
 local nextStates = table.create(width * height, 0)
 
@@ -40,11 +45,25 @@ function updateState(col, row, state)
 	if row < 1 then
 		row += height
 	end
-	if row > width then
+	if row > height then
 		row -= height
 	end
 	
-	nextStates[(row-1)*width+(col-1)+1] = state
+	local currentState = cellStates[(row-1)*width+(col-1)+1]
+	nextStates[(row-1)*width+(col-1)+1] = math.max(currentState + state, 0)
+end
+
+function newRandomState()
+	-- roll a die, if it comes up one way, the thing is dead, otherwise choose a random live state
+	if math.random() < initialAlive then
+		return newLiveState()
+	else
+		return 0
+	end
+end
+
+function newLiveState()
+	return math.floor(math.random() * maxHealth + 1)
 end
 
 -- Get the state of a single cell.
@@ -59,7 +78,7 @@ function getState(col, row)
 	if row < 1 then
 		row += height
 	end
-	if row > width then
+	if row > height then
 		row -= height
 	end
 	
@@ -70,13 +89,13 @@ function getNeighborCount(col, row)
 	local count = 0
 	for r = row - 1, row + 1, 1 do
 		for c = col - 1, col + 1, 1 do
-			if getState(c, r) then
+			if getState(c, r) > 0 then
 				count += 1
 			end
 		end
 	end
 	
-	if getState(col, row) then
+	if getState(col, row) > 0 then
 		count -= 1
 	end
 	
@@ -88,17 +107,17 @@ function updateStates()
 		for col = 1, width, 1 do
 			local state = getState(col, row)
 			local count = getNeighborCount(col, row)
-			if state then
+			if state > 0 then
 				if count == 2 or count == 3 then
-					updateState(col, row, true)
+					updateState(col, row, 0)
 				else
-					updateState(col, row, false)
+					updateState(col, row, -1)
 				end
 			else
 				if count == 3 then
-					updateState(col, row, true)
+					updateState(col, row, newLiveState())
 				else
-					updateState(col, row, false)
+					updateState(col, row, -1)
 				end
 			end
 		end
@@ -113,10 +132,11 @@ function drawStates()
 	for row = 1, height, 1 do
 		for col = 1, width, 1 do
 			local state = getState(col, row)
-			if state then
-				gfx.fillRect((col - 1)*size, (row - 1)*size, size, size)
-			else
+			if state > 0 then
 				gfx.drawRect((col - 1)*size, (row - 1)*size, size, size)
+				gfx.fillRect((col - 1)*size + (size / 2 - 1), (row - 1)*size + (size / 2 - 1), 2, 2)
+			else
+				gfx.fillRect((col - 1)*size, (row - 1)*size, size, size)
 			end
 		end
 	end
@@ -130,7 +150,8 @@ function setup()
 	gfx.setLineWidth(1)
 	
 	for i = 1, width * height, 1 do
-		cellStates[i] = math.random() > 0.8
+		cellStates[i] = newRandomState()
+		nextStates[i] = cellStates[i]
 	end
 end
 
@@ -151,4 +172,5 @@ function playdate.update()
 	updateStates()
 	
 	playdate.timer.updateTimers()
+	playdate.wait(stepTime)
 end
